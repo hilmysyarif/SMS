121f16878173ec9eeb435d9a80ab6801944b5a29
dev1 <dev1@junctiontech.in>
2015-09-19 15:54:39 +0530

    add to index

---
+++ application/views/help.php
@@ -0,0 +1,29 @@
+
+<html>
+<head>
+<title>Help For Guidance</title>
+<link rel="shortcut icon" href="<?=base_url();?>assets/images/junctionerplogo.png">
+</head>
+<body >
+<div>
+<h1>Please Follow Instruction To Use Software</h1>
+<p><h3>1 Step: After Login , The First Page You see Dashboard , There On Top You See Settings . Click On Setting And There on First There Is General Setting Option . Now Click On General Setting And Register Your School. Fill All Information.</h3> </p>
+<p><image src="<?=base_url();?>assets/images/help1.png"/></p>
+<p><image src="<?=base_url();?>assets/images/help2.png"/></p>
+<p><h3>2 Step: After Registration You See On Top Session . Now Select Your School Session</h3></p>
+<p><image src="<?=base_url();?>assets/images/help3.png"/></p>
+<p><image src="<?=base_url();?>assets/images/help4.png"/></p>
+<p><h3>3 Step: Now In Settings There You See Master Entry. Create All You Need Like Blood Group, Caste, User Type etc...</h3></p>
+<p><image src="<?=base_url();?>assets/images/help5.png"/></p>
+<p><h3>4 Step: In Settings Create All Account, Class, Section, Subjects, Exams, Fee etc..</h3></p>
+<p><image src="<?=base_url();?>assets/images/help6.png"/></p>
+<p><image src="<?=base_url();?>assets/images/help7.png"/></p>
+<p><h3>5 Step: Register Your Students From Admission.</h3><p/>
+<p><image src="<?=base_url();?>assets/images/help8.png"/></p>
+<p><h3>6 Step: After Registration Of Students . In Admission Do Admission Of Them.</h3></p>
+<p><image src="<?=base_url();?>assets/images/help9.png"/></p>
+</div>
+</body>
+</html>
+
+
