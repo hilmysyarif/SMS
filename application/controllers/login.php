fe714b150aabe6fd768abac49e58197103d36a06
ankit <dev2@junctiontech.in>
2015-10-27 16:48:13 +0530

    test for androide application

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -28,7 +28,7 @@ class Login extends CI_Controller {
 	function login_user($info=false)
 	{		
 		$json= $_GET['json'];
-		$json_data=json_decode($json);
+		$json_data=json_decode($json);print_r($json_data);die;
 		$this->session->set_userdata('url',$json_data->url);
 		$this->session->set_userdata('db_name',$json_data->database_name);
 		$this->session->userdata('db_name');
