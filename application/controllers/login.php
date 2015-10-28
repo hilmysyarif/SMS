a23734a2fedc0a6841333632cc0e7c11e4291efe
ankit <dev2@junctiontech.in>
2015-10-28 17:59:58 +0530

    final test for androide app

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -45,12 +45,10 @@ function login_user($info=false)
 					if(isset($row->UserType)&& $row->UserType!=='0')
 					{
 						$row=$this->login_model->userType($row->UserType);
-						print_r($row);die;
-						$userType=$row->MasterEntryValue;
 						$data=array(
 								'status'=>'200',
 								'result'=>'success',
-								'userType'=>$userType,
+								'userType'=>$row->MasterEntryValue,
 						);
 						print_r($data);die;
 					}
