1ff9e2ea3b5e912ef5eb932a8b5164c1fe32241a
ankit <dev2@junctiontech.in>
2015-11-05 22:34:23 +0530

    commit

--- application/models/user_management_model.php
+++ application/models/user_management_model.php
@@ -17,8 +17,6 @@ class User_management_model extends CI_Model{
     function clone_db($database_name=false,$organization_id=false)
     {
     	$qry=$this->db->query('CREATE DATABASE '.$database_name);
-		$this->session->set_userdata('db_name',$database_name);
-    	$this->session->userdata('db_name');
     	if($_SERVER['HTTP_HOST']=="localhost"){
     		//$dbname=$database_name;
     		$password="";
