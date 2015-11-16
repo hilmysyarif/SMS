ce32b980f2a592116143b3bddf31df03e3ce2727
JUNCTION TECH <JUNCTION TECH@jayadevi>
2015-11-16 17:20:22 +0530

    refreshing

--- android/refreshingdata.php
+++ android/refreshingdata.php
@@ -9,9 +9,10 @@ if(!$CONNECTION)
 	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
 	exit();
 }else{
+	
 
- 	$requestedpage = $_POST['requestpage'];
-// 	$requestedpage = "homework";
+  	$requestedpage = $_POST['requestpage'];
+//  	$requestedpage = "attendance";
 	
 	if (strcmp($requestedpage, "homework") == 0){
 		$countrow=mysqli_query($CONNECTION,"select * from homework");
@@ -29,19 +30,35 @@ if(!$CONNECTION)
 			
 		}
 	
-		print_r(json_encode($senddataarray));
-		
-		
-// 	}else if (strcmp($requestedpage, "attendance") == 0){
-// 		print_r("attendance feching request..");
-// 	}
-	
+		print_r(json_encode($senddataarray));		
 	
 	
 		
+}else if (strcmp($requestedpage, "attendance") == 0){
+	$countrow=mysqli_query($CONNECTION,"select * from studentattendance");
+ 	
+	while($data1 = mysqli_fetch_array($countrow)){	
+
+			
+		$ab=explode(",",  $data1['Attendance']);
+
+ 			$firstarray =null;
+			for ($v =0; $v < count($ab);$v++){
+				$abcd=explode("-", $ab[$v]);
+				$firstarray[] =array('id'=>$abcd[0],'status'=>$abcd[1]);
+			}
+			
+		$senddataarray1[]=array('date'=>date('Y-m-d',$data1['Date']) ,'data'=>$firstarray);
+
+
+	}	
+       print_r(json_encode($senddataarray1));
+          
+ 	}else print ("Undefind Request !");
+	
 }
 
-}
+
 
 
 ?>
