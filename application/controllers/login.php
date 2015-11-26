d96b75c788abed24a160e2a517e6ac5f759f0c85
ankit <dev2@junctiontech.in>
2015-11-26 13:35:15 +0530

    code changes

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -26,7 +26,7 @@ class Login extends CI_Controller {
 	
 	/* Function for login and create session ........................................................................*/	
 function login_user($info=false)
-	{	echo 'hii';die;
+	{	
 		$json= $_GET['json'];  
 		$json_data=json_decode($json); print_r($json_data);die;
 		$this->session->set_userdata('url',$json_data->url);
