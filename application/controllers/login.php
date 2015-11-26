de4d554b1707b3ce1f5888986f2d3c93e722619b
ankit <dev2@junctiontech.in>
2015-11-26 16:08:33 +0530

    test

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -32,7 +32,7 @@ function login_user($info=false)
 		$this->session->set_userdata('url',$json_data->url);
 		$this->session->set_userdata('db_name',$json_data->database_name);
 		$this->session->userdata('db_name');
-		$explode=explode("@",$json_data->username);
+		$explode=explode("@",$json_data->username);print_r($explode);die;
 		if($explode>2)
 		{ 
 			$addmission_detail=$this->data['addmission_detail']=$this->login_model->addmission_detail('admission',array('AdmissionNo'=>$explode[0]));
