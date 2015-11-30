a023e591c1aa6e8360a46eff559a6f862717bfc9
ankit <dev2@junctiontech.in>
2015-11-30 16:09:33 +0530

    changes code for delete application

--- application/controllers/user_management.php
+++ application/controllers/user_management.php
@@ -93,13 +93,13 @@ class User_management extends CI_Controller {
 		$var=json_decode($json_data);
 		$database_name=$var->db_name;
 		$this->session->set_userdata('db_name',$database_name);
-		//$delete_function=$this->data['delete_function']=$this->user_management_model->delete_function($database_name);
+		$delete_function=$this->data['delete_function']=$this->user_management_model->delete_function($database_name);
 		$data=array(
 					'session'=>$var->session,
 					'status'=>'success',
 					'reg_app_id'=>$var->reg_app_id,
 					);
-		$json=json_encode($data);print_r($json);die;
+		$json=json_encode($data);
 		redirect('http://junctiondev.cloudapp.net/appmanager/admin_panel/delete_app_org?json='.$json);
 	}
 }
