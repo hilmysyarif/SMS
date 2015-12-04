90e1ab4f432f2ee5f837a84fa28a4167822031a3
JUNCTION TECH <JUNCTION TECH@jayadevi>
2015-12-04 15:04:52 +0530

    msg 1

--- android/sendmessage.php
+++ android/sendmessage.php
@@ -71,14 +71,15 @@ if(!$CONNECTION)
 // 			print_r($data1);
 		
 			$countrow1=mysqli_query($CONNECTION,"select * from staff where StaffPosition='$data1'");
-			
+			print_r($countrow1);
 		       while ($data2 = mysqli_fetch_array($countrow1))
 		       {
+		       	print_r($data2);
 		         	$senddataarray[] = array('TeacherID'=>$data2['StaffId'], 'TeacherName'=>$data2['StaffName']);
 		       }
 			}
 		
- 		print_r(json_encode($senddataarray));	
+//  		print_r(json_encode($senddataarray));	
 	}
 	
 	
