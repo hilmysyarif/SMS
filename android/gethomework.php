2a59a5281c78b03bb0fd9ee48ba9e916f0bfe796
JUNCTION TECH <JUNCTION TECH@jayadevi>
2015-12-09 11:58:30 +0530

    homework android update

--- android/gethomework.php
+++ android/gethomework.php
@@ -25,31 +25,31 @@ foreach($dataarray['SchoolData']['SchoolHomeWork'] as $clsdataarray){
 			
 			foreach ($datearray['SubjectWork'] as $subjectarray){	
 			
-
+				
 				    $classID = $clsdataarray['ClassID'];
 					$sectionID = $secdataarray['SectionID'];
-					$date = $datearray['Date'];
+					$date = strtotime($datearray['Date']);
 					$subID = $subjectarray['SubjectId'];
 					$homework = $subjectarray['HomeWork'];	
 					
-        		$query="Select * from homework where classid='$classID' AND sectionid='$sectionID' AND subjectid='$subID' AND createdon='$date'";
+        		$query="Select * from homework where classid='$classID' AND sectionid='$sectionID' AND subjectid='$subID' AND dateofhomework='$date'";
 				$countrow =	mysqli_query($CONNECTION,$query);
 				
 				if (mysqli_num_rows($countrow)!=0){
 					
-					$sql = "UPDATE homework SET homework='$homework' WHERE classid='$classID' AND sectionid='$sectionID' AND subjectid='$subID' AND createdon='$date'";
+					$sql = "UPDATE homework SET homework='$homework' WHERE classid='$classID' AND sectionid='$sectionID' AND subjectid='$subID' AND dateofhomework='$date'";
 				$resultupdate =	mysqli_query($CONNECTION,$sql);
 				if ($resultupdate){
-					$Message="updated successfully..!!!!";
+					$Message="homework updated successfully..!!!!";
 					print_r($Message);
 				}
 					
 					
 				}else{
-					$queryInsert="insert into homework(classid,sectionid,subjectid,homework,createdon) values('$classID','$sectionID','$subID','$homework','$date') ";
+					$queryInsert="insert into homework(classid,sectionid,subjectid,homework,dateofhomework) values('$classID','$sectionID','$subID','$homework','$date') ";
 				
 					if (mysqli_query($CONNECTION,$queryInsert))
-					$Message="Attendance Added successfully!!";
+					$Message="homework Added successfully!!";
 					print_r($Message);
 					
 				}	
