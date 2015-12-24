f7c487a11f274b0a5dfe2ce7a70495ccbc630d94
JUNCTION TECH <JUNCTION TECH@jayadevi>
2015-12-24 11:27:18 +0530

    ritu

--- android/examResult.php
+++ android/examResult.php
@@ -38,18 +38,36 @@ if(!$CONNECTION)
 		$student_id =$resultsarray['student_id'];
 		$grades =$resultsarray['grade'];
 		$section_id =$resultsarray['section_id'];
-		$examName =$resultsarray['examName'];
+		$examName = strtotime($resultsarray['examName']);		
 		$subjectid =$resultsarray['subjectid'];
 		$evaluated_by =$resultsarray['evaluated_by'];
 			
-			$queryInsert="insert into examdetails(Exam_Type,Exam_Detail_Status,Section_Id,Student_Id,Session,Subject_Id,Marks_Obtain,Max_Marks,Result,Grade,Remarks,DateOfExam,Evaluated_By) values('$examName','Active','$section_id','$student_id','$obtained_marks','$maximum_marks','$resultID','$grades','','$exam_date','$evaluated_by') ";
-			mysqli_query($CONNECTION,$queryInsert);
 			
-			if (mysql_affected_rows()>=0){
-				print_r("inserted");
-						print_r($examName);
-						
+		
+		$query="Select Exam_Detail_Id from examdetails where Exam_Type='$examName' AND Section_Id='$section_id' AND Subject_Id='$subjectid' AND Student_Id='$student_id' AND DateOfExam='$exam_date' AND Session='$session'";
+		$countrow =	mysqli_query($CONNECTION,$query);
+		
+		if (mysqli_num_rows($countrow)!=0){
+			$data1 = mysqli_fetch_array($countrow);
+			
+			$Exam_Detail_Id = $data1['Exam_Detail_Id'];
+			
+			$sql = "UPDATE examdetails SET Marks_Obtain='$obtained_marks', Max_Marks='$maximum_marks',Result='$resultID',Grade='$grades',Evaluated_By='$evaluated_by' WHERE Exam_Detail_Id='$Exam_Detail_Id'";
+			$resultupdate =	mysqli_query($CONNECTION,$sql);
+			if ($resultupdate){
+				$Message="Exam Result updated successfully..!!!!";
+				print_r($Message);
 			}
+		
+				
+		}else {
+
+			$queryInsert="insert into examdetails(Exam_Type,Exam_Detail_Status,Section_Id,Student_Id,Session,Subject_Id,Marks_Obtain,Max_Marks,Result,Grade,Remarks,DateOfExam,Evaluated_By) values('$examName','Active','$section_id','$student_id','$obtained_marks','$maximum_marks','$resultID','$grades','','$exam_date','$evaluated_by') ";
+					
+			if (mysqli_query($CONNECTION,$queryInsert))
+				$Message="Exam Result Added successfully!!";
+			print_r($Message);
+				
 		}
 				
 			
