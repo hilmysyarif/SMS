8a7600e9b3be0c055605016309e93348aa53aaee
nabeela <nabans1811@gmail.com>
2016-03-30 14:41:02 +0530

    hi

--- application/controllers/admission.php
+++ application/controllers/admission.php
@@ -496,13 +496,13 @@ class Admission extends CI_Controller {
 								
 								
 								
-									if($emapData[$gt]==$section2[3]->MasterEntryValue)
+									if($emapData[$gt]==$section2[14]->MasterEntryValue)
 									{
-										$var=$section2[3]->MasterEntryId;	
+										$var=$section2[14]->MasterEntryId;	
 									}
-									if($emapData[$gt]==$section2[5]->MasterEntryValue)
+									if($emapData[$gt]==$section2[15]->MasterEntryValue)
 									{
-										$var=$section2[5]->MasterEntryId;
+										$var=$section2[15]->MasterEntryId;
 									}
 									if($var==''){$var='NULL';}
 									$k=0;
@@ -639,7 +639,7 @@ class Admission extends CI_Controller {
 						$section1=$this->data['detail1']=$this->master_model->section1($emapData[5],$emapData[4]);
 						
 						$section2=$this->data['detail2']=$this->master_model->section2();
-						print_r($section2);die;
+
 						if(isset($section1[0]->ClassName) && isset($section1[0]->SectionName) && !empty($section1[0]->SectionName) && !empty($section1[0]->ClassName))
 							
 							{ 	 
@@ -689,13 +689,13 @@ class Admission extends CI_Controller {
 								
 								
 								
-									if($emapData[6]==$section2[3]->MasterEntryValue)
+									if($emapData[6]==$section2[14]->MasterEntryValue)
 									{
-										$var=$section2[3]->MasterEntryId;	
+										$var=$section2[14]->MasterEntryId;	
 									}
-									if($emapData[6]==$section2[5]->MasterEntryValue)
+									if($emapData[6]==$section2[15]->MasterEntryValue)
 									{
-										$var=$section2[5]->MasterEntryId;
+										$var=$section2[15]->MasterEntryId;
 									}
 									if($var==''){$var='NULL';}
 									$k=0;
