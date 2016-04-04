27fb3c6e80dbb08cc2d5e48d9a5301cc710611a2
nabeela <nabans1811@gmail.com>
2016-04-04 16:07:31 +0530

    hi

---
+++ application/views/setuppg.php
@@ -0,0 +1,37 @@
+
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 col-lg-12 col-md-12">
+   
+  <h2 style ="margin-top:100px"> Setup Guide</h2>
+  <h4 style ="margin-top:50px;margin-bottom:20px">This setup guide page will be available in help menu in rightmost corner</h4>
+  <ul>
+  <li style ="margin-top:50px;margin-bottom:20px"> Watch this video for complete setup guide</li>
+   
+   <video width="350" height="260" controls>
+  <source src="<?=base_url();?>sample.mp4" type="video/mp4">
+
+Your browser does not support the video tag.
+</video>
+ 					
+   
+
+   <li style ="margin-top:50px;margin-bottom:20px">To download instruction file for uploading of student detail through Excel sheet</li>
+    <form role="form" method="post" action="<?=base_url();?>dashboard/download">
+	<div class="col-sm-12">
+		
+           <input type="submit" class="btn btn-custom" name="submit" value="Download File" />
+	</div>
+       </form>
+	   
+ <div class="col-sm-12" >
+
+<a href="<?=base_url();?>master/generalsetting" class="btn btn-primary btn-lg" style="margin-top:10px ;background-color:black;float:right">Next</a>
+</div>
+      
+ </div>
+  </div>	
+</div>								
+
+</body>
