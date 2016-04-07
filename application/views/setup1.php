0e87653eed13f7afd6c051ca484baa0efcb39fb5
nabeela <nabans1811@gmail.com>
2016-04-07 13:19:10 +0530

    hi

--- application/views/setup1.php
+++ application/views/setup1.php
@@ -68,7 +68,7 @@
 
 
 <div class="col-lg-12 col-md-12 col-sm-12" style="margin-top:40px">
-<form role="form" onsubmit="return checkForm()" method="post" style="margin-bottom:60px" action="<?=base_url();?>dashboard/insert1" onsubmit="return checkCheckBoxes(this);">
+<form role="form"  method="post" style="margin-bottom:60px" action="<?=base_url();?>dashboard/insert1" onsubmit="return checkCheckBoxes(this);">
 <input tabindex="5" type="checkbox" name="terms" class="icheck-11" id="minimal-checkbox-1-11">
 <label for="minimal-checkbox-1-11" style="font-size:18px;font-family:Comic Sans MS">I agree to the Junction Software terms and privacy policy</label>
 <br>
