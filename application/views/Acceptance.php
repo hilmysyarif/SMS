a1c07f4ff196ebc72c30954136e4bfc971ec37d0
nabeela <nabans1811@gmail.com>
2016-04-07 17:22:06 +0530

    hi

--- application/views/Acceptance.php
+++ application/views/Acceptance.php
@@ -5,11 +5,11 @@
 						<H4>Are you ready to work on School Management Software</h4>
 						</div>
 <div class="col-md-2">
-						<a href="<?=base_url();?>master/acceptance" class="btn btn-primary btn-lg" style="margin-top:10px ;background-color:black;float:right">YES</a>
+						<a href="<?=base_url();?>dashboard" class="btn btn-primary btn-lg" style="margin-top:10px ;background-color:black;float:right">YES</a>
 						</div>
 						
 <div class="col-md-2">
-						<a href="<?=base_url();?>master/acceptance" class="btn btn-primary btn-lg" style="margin-top:10px ;background-color:black;float:right">NO</a>
+						<a href="<?=base_url();?>dashboard/setuppg" class="btn btn-primary btn-lg" style="margin-top:10px ;background-color:black;float:right">NO</a>
 						</div>
 						</div>
 </div></div>
