2ee0fc26d85881ed6b61b7c987e29b1ff26361a6
nabeela <nabans1811@gmail.com>
2016-04-08 16:53:15 +0530

    hi

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -195,7 +195,7 @@ class Dashboard extends CI_Controller {
 		
 		$generalsetting=$this->data['generalsetting']=$this->Dashboard_model->generalsetting();	
 		 $agreement_detail=$this->data['detail']=$this->Dashboard_model->agreement($Studentspassword,$Parentspassword);
-
+print_r( $agreement_detail);die;
 		 if(((empty($generalsetting)) && $this->info['UserType']==0 ) || ((empty($agreement_detail)) && $this->info['UserType']!=0  ))
 		{
 			
