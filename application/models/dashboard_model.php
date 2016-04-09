21209912d8e0a4b434fe471b9912db54c249d052
nabeela <nabans1811@gmail.com>
2016-04-09 17:59:53 +0530

    hi

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -109,14 +109,14 @@ class Dashboard_model extends CI_Model {
 		return $query->Result();
 	
 	}
-	function staff_agreement($checkid=false)
+	/* function staff_agreement($checkid=false)
 	{ 
 		$query=$this->db->query("select Sterms from registration,admission where Sterms='Accepted' and admission.AdmissionId='$checkid'
 										and registration.RegistrationId=admission.RegistrationId");
 			
 		return $query->Result();
 	
-	}
+	} */
 	
 		function generalsetting()
     {
@@ -140,11 +140,11 @@ class Dashboard_model extends CI_Model {
 										and registration.RegistrationId=admission.RegistrationId");
    }
    
-      function update_studentterms($checkid=false)
+/*       function update_studentterms($checkid=false)
    {
   	$query=$this->db->query("Update registration,admission set Sterms='Accepted' where admission.AdmissionId='$checkid'
 										and registration.RegistrationId=admission.RegistrationId");
-   }
+   } */
 
 }
 
