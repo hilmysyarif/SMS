51cb9637ef7b1aa9f641051bcc285759b6f273ca
nabeela <nabans1811@gmail.com>
2016-04-09 12:02:53 +0530

    test commit again

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -198,7 +198,7 @@ class Dashboard extends CI_Controller {
 		
 		$generalsetting=$this->data['generalsetting']=$this->Dashboard_model->generalsetting();	
 		
-		$agreement_detail=$this->data['detail']=$this->Dashboard_model->agreement();
+		$agreement_detail=$this->data['detail']=$this->Dashboard_model->agreement($this->info['user_id']);
 
 		 if(((empty($generalsetting)) && $this->info['UserType']==0 ) || ((empty($agreement_detail)) && $this->info['UserType']!=0  ))
 		{
