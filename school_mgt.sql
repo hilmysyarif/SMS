94186e5f69c62ac632eed60c5d40e508f37643cd
nabeela <nabans1811@gmail.com>
2016-04-20 11:50:40 +0530

    hi

--- school_mgt.sql
+++ school_mgt.sql
@@ -423,7 +423,7 @@ CREATE TABLE IF NOT EXISTS `generalsetting` (
   `SchoolStartDate` varchar(20) NOT NULL,
   `SchoolName` varchar(500) NOT NULL,
   `SchoolMoto` varchar(500) NOT NULL,
-  `Image` varchar(500) NOT NULL,
+  `Logo` varchar(500) NOT NULL,
   `SchoolAddress` text NOT NULL,
   `City` varchar(100) NOT NULL,
   `District` varchar(100) NOT NULL,
