848afe8b28278adf9ba4af96de9a456a33d75dbd
JUNCTION TECH <JUNCTION TECH@jayadevi>
2016-04-26 16:13:07 +0530

    s

--- android/ontimeshedular.php
+++ android/ontimeshedular.php
@@ -22,8 +22,8 @@ $exm = "April 25 2016 12:13 PM";
 $aa = strtotime('April 20 2016');
 $newformat = date('Y-m-d',$aa);
 
-$datetime1 = new DateTime('2009-10-11');
-$datetime2 = new DateTime('2009-10-13');
+$datetime1 = new DateTime('April 20 2016');
+$datetime2 = new DateTime('$newformat');
 $interval = $datetime1->diff($datetime2);
 echo $interval->format('%R%a days');
 
